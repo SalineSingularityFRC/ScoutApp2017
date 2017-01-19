572249bd2dd73827d38d1efade7f2b20a77432b1
Seve4 <gseve08@gmail.com>
2017-01-19 18:46:08 -0500

    This a test.

--- app/src/main/java/scouting2017/salinerobotics/org/a2017scoutingapp/MainMenu.java
+++ app/src/main/java/scouting2017/salinerobotics/org/a2017scoutingapp/MainMenu.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainMenu extends AppCompatActivity {
-
+//Gaven was here :D
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
