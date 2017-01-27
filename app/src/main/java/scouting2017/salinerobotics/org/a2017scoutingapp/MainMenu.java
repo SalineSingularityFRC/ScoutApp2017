f8fd0979c12c3dd98d06475357fbc8826bd93714
Seve4 <gseve08@gmail.com>
2017-01-26 19:39:47 -0500

    Removed Team Activity Fixed Errors Created Login Activity

--- app/src/main/java/scouting2017/salinerobotics/org/a2017scoutingapp/MainMenu.java
+++ app/src/main/java/scouting2017/salinerobotics/org/a2017scoutingapp/MainMenu.java
@@ -15,8 +15,8 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
     }
 
     @Override
