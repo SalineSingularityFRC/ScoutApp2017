0c156c4bd06f65e6caa9ea8a77f4fbf19d17bdfe
Grant D <gringrant@gmail.com>
2017-02-03 19:51:11 -0500

    Linked buttons and text field to code.

--- app/src/main/java/scouting2017/salinerobotics/org/a2017scoutingapp/MainMenu.java
+++ app/src/main/java/scouting2017/salinerobotics/org/a2017scoutingapp/MainMenu.java
@@ -1,13 +1,10 @@
 package scouting2017.salinerobotics.org.a2017scoutingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -19,10 +16,8 @@ public class MainMenu extends AppCompatActivity {
         //setSupportActionBar(toolbar);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
-        return true;
+    public void onButtonGo(View v){
+        String user=((EditText)findViewById(R.id.txtboxUsername)).getText().toString();
+        int robot=Integer.parseInt(((EditText)findViewById(R.id.txtboxPassword)).getText().toString());
     }
 }
---
+++ app/src/main/java/scouting2017/salinerobotics/org/a2017scoutingapp/Scouting.java
@@ -0,0 +1,15 @@
+package scouting2017.salinerobotics.org.a2017scoutingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scouting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scouting);
+    }
+
+
+}
